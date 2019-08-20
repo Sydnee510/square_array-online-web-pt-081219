21971148047204a6dffc3708668bf63bbd036c65
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-20 23:12:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
-  counter = 1
-  #array = [1,2,3] 
+ # counter = 1
+  array = [1,2,3] 
+  new_array = []
   array.each do |numbers|
   puts numbers ** 2
-  counter += 1
+  #counter += 1
   
    end
-   result
 end
